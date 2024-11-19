269a592e4d2ef72e23eb79da34b0208ad5d963bb
apelic <apelic92@gmail.com>
2024-11-18 19:18:58 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+
+
+/* 
+Psuedocode: 
+1. Create .html file
+2. Declare .hmtl file using '!' emmet
+3. Create .js file
+4. Link .js file to .html file in the head tag, using: <script src="scriptfile.js"></script>
+5. In the .js file, declare the variables for each calculation, using:
+
+let str1 = "You have received this message 
+because you have been chosen to open an important vault. Here is the 
+secret combination:"
+
+let num1 = 20 / 2;
+let num2 = 20 * 2;
+let num3 = 30 + 9;
+let space = " ";
+
+
+6. In the .js file, create a dialouge box with the string attached, using:
+
+alert(str1 + space + num1 + space + num2 + space + num3) 
+
+*Use this element within the <script> tag*
+
+*/
+
+let str1 = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+let num1 = 20 / 2;
+let num2 = 20 * 2;
+let num3 = 30 + 9;
+let space = " ";
+
+/*use 'let' element to declare arithmatic variables and intro string. */
+
+alert(str1 + space + num1 + space + num2 + space + num3)
+
+/*Cobine string and artihmatic variables using the 'alert' 
+element, and using '+' to create completed string, which 
+will be our full message. */
+
+
+
+
+console.log('Hi')
+
+
+
+
+
+
+
+
+
+
